d8920d9f4527ce26628bd83b73321c45ac3044df
hectorj <hector.jusforgues@gmail.com>
2017-12-21 16:21:52 +0100

    Sitemap POC

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new WizaplaceFrontBundle\WizaplaceFrontBundle(),
+            new Dpn\XmlSitemapBundle\DpnXmlSitemapBundle(),
             new AppBundle\AppBundle(),
         ];
 
